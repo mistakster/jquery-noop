610a0c21c245367a15688a9cfdc5cfc6d9aa98cd
mista_k <mistakster@gmail.com>
2013-01-17 09:21:48 +0600

    noop plugin

---
+++ jquery-noop.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * jQuery Noop -- The smallest plugin ever
+ * http://noteskeeper.ru/381/
+ *
+ * Copyright 2012, 2013 Vladimir Kuznetsov <mistakster@gmail.com>
+ * Released under a Creative Commons Attribution 3.0 Unported License
+ * http://creativecommons.org/licenses/by/3.0/
+ *
+ */
+jQuery.fn.noop = function () {
+    return this;
+};
